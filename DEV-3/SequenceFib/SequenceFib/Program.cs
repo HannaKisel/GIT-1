98bce240b4326afcb4f76dfba699ef352c8e1b66
Moroz Hanna <amoroz408@gmail.com>
2017-06-23 18:56:58 +0300

    Create new project with cin and cout

---
+++ DEV-3/SequenceFib/SequenceFib/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SequenceFib
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please, enter number");
+            String inputString = Console.ReadLine();
+            int numbFibonacci = int.Parse(inputString);
+            if (numbFibonacci < 0)
+            {
+                Console.WriteLine("I can't check this number");
+
+            }
+            Console.WriteLine("For exit press any button");
+            Console.ReadKey();
+        }
+    }
+
+}
