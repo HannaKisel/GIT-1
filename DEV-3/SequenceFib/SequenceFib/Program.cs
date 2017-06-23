98861d77826a51faf1b43b90bef5872b6f553549
Moroz Hanna <amoroz408@gmail.com>
2017-06-23 19:01:06 +0300

    Task with Fibonacci number

--- DEV-3/SequenceFib/SequenceFib/Program.cs
+++ DEV-3/SequenceFib/SequenceFib/Program.cs
@@ -6,17 +6,37 @@ namespace SequenceFib
     {
         static void Main()
         {
-            Console.WriteLine("Please, enter number");
+            Console.Write("Please, enter number ");
             String inputString = Console.ReadLine();
             int numbFibonacci = int.Parse(inputString);
             if (numbFibonacci < 0)
             {
                 Console.WriteLine("I can't check this number");
-
             }
+            int[] array = new int[numbFibonacci + 3];
+            array[0] = 0;
+            array[1] = 1;
+            bool check = false;
+            Console.WriteLine(" Fibonacci sequence :  ");
+            for (int i = 0; i <= numbFibonacci; i++)
+            {
+                array[i + 2] = array[i] + array[i + 1];
+                Console.Write(array[i] + " ");
+                if (numbFibonacci == array[i])
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("This's  Fibonacci number = " + numbFibonacci);
+                    check = true;
+                    break;
+                }
+            }
+            if (check == false)
+            {
+                Console.WriteLine("\nThis is't Fibonacci number ");
+            }
+            Console.WriteLine(" ");
             Console.WriteLine("For exit press any button");
             Console.ReadKey();
         }
     }
-
 }
