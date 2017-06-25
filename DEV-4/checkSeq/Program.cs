1d2a153533981a5121070f3addd43206712468f1
Moroz Hanna <amoroz408@gmail.com>
2017-06-25 17:31:34 +0300

    Remade the project

---
+++ DEV-4/checkSeq/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace checkSeq
+{
+    class checkSequence
+    {
+        static void Main(string[] args)
+        {
+            bool Checking = true;
+            while (Checking)
+            {
+                try
+                {
+                    Console.Write("Size of your sequence is n = ");
+                    String sizeArray = Console.ReadLine();
+                    int size = int.Parse(sizeArray);
+                    int[] array = new int[size];
+                    string stringAr;
+                    if (size == 1)
+                    {
+                        Console.WriteLine("This sequence is not nondecreasing and is't decreasing ");
+                        continue;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < size; i++)
+                        {
+                            Console.Write("Enter a[" + (i + 1) + "] = ");
+                            stringAr = Console.ReadLine();
+                            array[i] = int.Parse(stringAr);
+                        }
+                        Console.Write("Your sequence is : ");
+                        for (int i = 0; i < size; i++)
+                        {
+                            Console.Write(array[i] + " ");
+                        }
+                        int chek = 0;
+                        for (int i = 1; i < size; i++)
+                        {
+                            if (array[i] >= array[i - 1])
+                            {
+                                chek++;
+                            }
+                        }
+                        if (chek == 0)
+                        {
+                            Console.WriteLine("\n This sequence is decreasing ");
+                        }
+                        else
+                        { Console.WriteLine("\n This sequence is nondecreasing "); }
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("It is not a number. Try again");
+                    continue;
+                }
+                Checking = false;
+            }
+        }
+    }
+}
