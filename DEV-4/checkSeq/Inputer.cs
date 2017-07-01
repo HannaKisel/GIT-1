1cbc6475d51ec01d2c46e8843d9e488469766130
Moroz Hanna <amoroz408@gmail.com>
2017-07-01 14:41:27 +0300

    modifi DEV-4 and delete usless return

--- DEV-4/checkSeq/Inputer.cs
+++ DEV-4/checkSeq/Inputer.cs
@@ -6,26 +6,26 @@ namespace checkSeq
     {
         public int[] input(string[] comLineArgs)
         {
+            int[] array = null;
             if (comLineArgs.Length == 0)
             {
                 Console.Write("Enter array from console = ");
                 string[] stringArray = Console.ReadLine().Split(new char[] { ',', ';' });
-                int[] array = new int[stringArray.Length];
+                array = new int[stringArray.Length];
                 for (int i = 0; i < stringArray.Length; i++)
                 {
                     array[i] = int.Parse(stringArray[i]);
                 }
-                return array;
             }
             else
             {
-                int[] array = new int[comLineArgs.Length];
+                array = new int[comLineArgs.Length];
                 for (int i = 0; i < comLineArgs.Length; i++)
                 {
                     array[i] = int.Parse(comLineArgs[i]);
                 }
-                return array;
             }
+            return array;
         }
     }
 }
