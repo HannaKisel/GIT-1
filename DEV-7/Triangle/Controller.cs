afb6f1acccc1893572772597b061c4960eb4dc22
Moroz Hanna <amoroz408@gmail.com>
2017-07-07 13:17:03 +0300

    Made full task

---
+++ DEV-7/Triangle/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Controller
+    {
+        const string UNCORRECTNUMB = "This numbers are uncorrect for tringle. Try again";
+
+        public bool control(double sideA, double sideB, double sideC)
+        {
+            bool checking = true;
+            if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+            {
+                checking = true;
+            }
+            else
+            {
+                Console.WriteLine(UNCORRECTNUMB);
+                checking = false;
+            }
+            return checking;
+        }
+    }
+}
---
+++ DEV-7/Triangle/Inputer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Inputer
+    {
+        const string POSERROR = "It is not a positive number. Try again";
+        const string SIDELENGTH = "Enter  the length of the side=";
+
+        public double input()
+        {
+            double side = 0;
+            bool reInput = true;
+            while (reInput == true)
+            {
+                Console.Write(SIDELENGTH);
+                side = double.Parse(Console.ReadLine());
+                if (side <= 0)
+                {
+                    Console.WriteLine(POSERROR);
+                    continue;
+                }
+                reInput = false;
+            }
+            return side;
+        }
+    }
+}
